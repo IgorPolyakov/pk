514d01279cd6643cb88f5b33d56e60d20258470b
Igor Polyakov <igor.polyakov@elecard.ru>
2018-04-13 15:48:59 +0700

    add user validation

--- task_four/app/models/user.rb
+++ task_four/app/models/user.rb
@@ -4,8 +4,7 @@ class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
   has_secure_password
-  validates_presence_of :login
-
+  validates :login, format: { with: /\d{3}.*_\w{2,3}/, message: 'like this 777_aaa, or 777-1_bbb' }
   field :login, type: String
   field :password_digest, type: String
 end

367d4d3abbe7c42fcf27cf0d51f85de51f15e132
IgorPolyakov <igorpolyakov@protonmail.com>
2018-04-19 23:46:04 +0700

    login must be uniq

--- task_four/app/models/user.rb
+++ task_four/app/models/user.rb
@@ -4,6 +4,7 @@ class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
   has_secure_password
+  validates :login, uniqueness: true
   validates :login, format: { with: /\d{3}.*_\w{2,3}/, message: 'like this 777_aaa, or 777-1_bbb' }
   field :login, type: String
   field :password_digest, type: String
